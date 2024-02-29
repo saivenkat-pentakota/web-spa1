ee7a46c7b6f6eda95f16d5e6932858a774aa0807
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2024-02-29 15:22:53 +0530

    added mainbody-text and mainbody-text-data

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./MainBody.css";
 import topRightArrow from "../images/top-right.png";
+import asterisk from "../images/asterisk.png";
 
 function MainBody() {
   return (
@@ -19,10 +20,31 @@ function MainBody() {
       </p>
       <div className="get-started">
         <p>
-            FOR BUSINESSES WHO NEED<br/>GOOD CREATIVE WITHOUT<br/>THE HASSLE OR HEADACHES
+          FOR BUSINESSES WHO NEED
+          <br />
+          GOOD CREATIVE WITHOUT
+          <br />
+          THE HASSLE OR HEADACHES
         </p>
-        <p className="getstarted-btn">GET STARTED</p>
-        
+        <div className="getstarted-btn">GET STARTED</div>
+      </div>
+      <div className="star">
+        <img className="topright-arrow" src={asterisk} alt="toprightarrow" />
+      </div>
+      <div className="mainbody-text">
+        <p>
+          NO AGENCY BLOAT.
+          <br />
+          NO FLAKY FREELANCERS.
+          <br />
+          JUST REAL <span style={{ color: "white" }}>GOOD</span> WORK.
+        </p>
+        <p className="mainbody-text-data">
+          That’s right. No contracts, No meetings, and no BS. Our service is
+          100% senior practitioner<br/> led, designed to give you nothing but
+          pixel-perfect work capable of winning the hearts,<br/> minds, and wallets
+          of your audiences. Don’t believe us? See some of it for yourself.
+        </p>
       </div>
     </div>
   );
